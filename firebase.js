a4fd0337ea1ffd2dfd97d93575294f17adfe053c
Anirban <116902917+anirban-25@users.noreply.github.com>
2023-03-07 22:37:38 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxfqobazw5lBf5GdUWOjCuPYz0KLZ5XcE",
+  authDomain: "wildfires-5aa96.firebaseapp.com",
+  projectId: "wildfires-5aa96",
+  storageBucket: "wildfires-5aa96.appspot.com",
+  messagingSenderId: "832366351480",
+  appId: "1:832366351480:web:20cfdb0c7de754186f498d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
